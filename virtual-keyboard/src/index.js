971f5659a528250d81aadc23e70216f0cb2d7f60
KatsiarynaZaviadzeyeva <katsiarynaappleby@gmail.com>
2022-05-08 21:48:00 +0300

    init: start virtual-keyboard-task

---
+++ virtual-keyboard/src/index.js
@@ -0,0 +1,4 @@
+﻿import './styles/styles.css';
+let element = document.createElement('div');
+element.innerHTML = '<h1>Virtual Keyboard</h1>';
+document.body.appendChild(element);
---
+++ virtual-keyboard/webpack.config.js
@@ -0,0 +1,29 @@
+﻿const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        server: "https",
+        https: true,
+        open: true,
+        port: 8080,
+        client: {
+            overlay: true,
+        },
+    },
+    plugins: [new HtmlWebpackPlugin({ template: './src/index.html' }),
+        new MiniCssExtractPlugin()
+    ],
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader, "css-loader"],
+        }, ],
+    },
+}
